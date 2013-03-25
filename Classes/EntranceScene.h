4378534bb4adec0ee13d9580785e83b9d34704e4
xophiix <xophiix@gmail.com>
2013-03-26 00:02:08 +0800

    win32 project created

---
+++ Classes/EntranceScene.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "cocos2d.h"
+
+class EntranceLayer : public cocos2d::CCLayer
+{
+public:
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();  
+
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::CCScene* scene();
+    
+    // a selector callback
+    void menuCloseCallback(CCObject* pSender);
+    
+    // implement the "static node()" method manually
+    CREATE_FUNC(EntranceLayer);
+};
